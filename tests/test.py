1b2ad42fd96bacc6b2c33988d391e6e7b8b6d927
Jung Yunho <a01049048063@gmail.com>
2024-07-17 10:27:43 +0900

    test: health check

--- tests/test.py
+++ tests/test.py
@@ -9,7 +9,7 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-def health_check():
+def test_health_check():
     response = client.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"messa1ge": "pong"}
